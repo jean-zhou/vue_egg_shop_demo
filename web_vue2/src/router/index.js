9fb42f0351b0d30fc4bd5a925aac2142b124c7ff
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2021-07-24 16:39:33 +0800

    创建testTree页面入口

--- web_vue2/src/router/index.js
+++ web_vue2/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TestTreePage from '../components/TestTreePage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/testTreePage',
+    name: 'TestTreePage',
+    component: TestTreePage
   },
   {
     path: '/about',
